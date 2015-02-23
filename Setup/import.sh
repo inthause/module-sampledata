d9655c779fe5f8765fbdac7d921e5f3df97811a4
Eric Hauswald <eric.hauswald@proximis.com>
2015-02-23 09:08:03 +0100

    Add topics

--- Setup/import.sh
+++ Setup/import.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+php bin/change.phar rbs_elasticsearch:client front --delete-all
 php bin/change.phar rbs_sampledata:set-defaults --webStore=first --billingArea=first --groupAttribute=first --section=first
-php bin/change.phar rbs_sampledata:import-sections Plugins/Modules/Rbs/Sampledata/Assets/Data/categories.json
+php bin/change.phar rbs_sampledata:import-sections Plugins/Modules/Rbs/Sampledata/Assets/Data/formatted-categories.json
 php bin/change.phar rbs_sampledata:import-products Plugins/Modules/Rbs/Sampledata/Assets/Data/products/apparel.json
 php bin/change.phar rbs_sampledata:import-products Plugins/Modules/Rbs/Sampledata/Assets/Data/products/appliance.json
 php bin/change.phar rbs_sampledata:import-products Plugins/Modules/Rbs/Sampledata/Assets/Data/products/baby.json
